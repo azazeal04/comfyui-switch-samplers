ca89b9b20ead6d3c21405c359f3dbd70b2b3c513
azazeal04 <132445160+azazeal04@users.noreply.github.com>
2025-10-04 14:33:25 +0200

    Add files via upload
    Add the CrossStepSwitchKSampler and CrossMultiStepKSampler nodes

--- __init__.py
+++ __init__.py
@@ -3,6 +3,8 @@ from .nodes import *
 NODE_CLASS_MAPPINGS = {
     "StepSwitchKSampler": StepSwitchKSampler,
     "MultiStepKSampler": MultiStepKSampler,
+    "CrossStepSwitchKSampler": CrossStepSwitchKSampler,
+    "CrossMultiStepKSampler": CrossMultiStepKSampler,
 }
 
 __all__ = list(NODE_CLASS_MAPPINGS.keys())
